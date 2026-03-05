1dd2f3298605c39db2b3a504898e43ff7289c864
AlExIa126hub <cazale126@gmail.com>
2026-03-05 19:57:34 +0200

    loaded dataset

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import streamlit as sl
+import pandas as pd
 
-# bla
-sl.title("Testing")
+path = r"C:\Users\cazal\OneDrive\Documents\FACULTATE\ANUL III\Sem 2 ASE\Pachete Software\proiect\PSW\data\listings.csv"
+df=pd.read_csv(path)
+
+sl.dataframe(df)
+
+
+
+info = df.info()
+type(info)
